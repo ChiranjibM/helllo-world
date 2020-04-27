abf5766c0f9a5228b19429468960ce092b13829c
MOHAPATRA <chiranjib.mohapatra@dxc.com>
2020-04-27 16:03:27 +0530

    modified

--- kk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-latest_commit = git log --diff-filter=D --summary | grep commit | head -1
-deleted_file_names=git show $latest_commit | grep a/ | awk '{print $2}' | grep a | sed 's/a//' | sed 's/^.//'
-echo deleted_file_names
-
